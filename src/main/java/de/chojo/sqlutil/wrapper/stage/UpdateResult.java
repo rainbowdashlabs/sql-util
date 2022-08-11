98e56cbb3be25cf8eec3d4562d6def63770cb454
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-08-11 19:50:08 +0200

    Add update result class

---
+++ src/main/java/de/chojo/sqlutil/wrapper/stage/UpdateResult.java
@@ -0,0 +1,27 @@
+package de.chojo.sqlutil.wrapper.stage;
+
+public class UpdateResult {
+    private final int rows;
+
+    public UpdateResult(int rows) {
+        this.rows = rows;
+    }
+
+    /**
+     * The amount of affected rows.
+     *
+     * @return changed rows
+     */
+    public int rows() {
+        return rows;
+    }
+
+    /**
+     * Checks if something has changed.
+     *
+     * @return true if something has changed
+     */
+    public boolean changed() {
+        return rows > 0;
+    }
+}
